4ac20159bb66a463b65389e984ba837308bf33ec
K <chutkinahnu@gmail.com>
2025-12-06 12:38:33 +0530

    Update main.cpp

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,18 +1,28 @@
-#include <FastLED.h>
+// moodcanvas firmware - early skeleton
+// dec 6: just setting things up, no real hardware tests yet
 
-#define NUM_LEDS 256
-#define DATA_PIN 5
-
-CRGB leds[NUM_LEDS];
+#include <Arduino.h>
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(40); // don't blind myself
+  Serial.begin(115200);
+  Serial.println("moodcanvas: starting up");
+
+  // TODO: init leds here later
+  // TODO: init sensors here later
 }
 
 void loop() {
-  for(int i = 0; i < NUM_LEDS; i++){
-    leds[i] = CHSV((i + millis()/10), 255, 200);
+  // for now just print something so we know the loop runs
+  static unsigned long lastPrint = 0;
+  unsigned long now = millis();
+
+  if (now - lastPrint > 1000) { // every 1 second
+    lastPrint = now;
+    Serial.println("loop tick - leds + sensors coming later");
   }
-  FastLED.show();
+
+  // later:
+  // 1. read sensors
+  // 2. map values to colors
+  // 3. update leds
 }
